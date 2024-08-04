4394ba8ce113ff836163df120a47ba7cf54921f9
kmoonn <3453863492@qq.com>
2024-08-04 17:30:17 +0800

    Refactoring code 2

--- methods/Pixel_Negative-Positive_Transformation.py
+++ methods/Pixel_Negative-Positive_Transformation.py
@@ -3,16 +3,84 @@
 # @Author : Kmoon_Hs
 # @File : Pixel_Negative-Positive_Transformation
 
-import itertools
 import random
 import numpy as np
 from matplotlib import pyplot as plt
 
-from datasets.mnist import MNIST
 from datasets.cifar10 import CIFAR10
+from datasets.mnist import MNIST
+
 
 class Pixel_Negative_Positive_Transformation:
-    def __init__(self):
-        pass
+    def __init__(self, image, Kc, Ks):
+        self.method_label = "PNPT"
+        self.image = image
+        if len(image.shape) == 3:
+            self.width, self.height, self.channels = image.shape
+            self.Kc = Kc
+            self.Ks = Ks
+        else:
+            self.width, self.height = image.shape
+            self.channels = 1
+            self.Kc = Kc[0]
+    def NP(self):
+        if self.channels == 3:
+            Kr, Kg, Kb = self.Kc
+            np.random.seed(Kr)
+            Ir = np.random.choice([0, 255], size=(self.width, self.height), p=[0.5, 0.5])
+            np.random.seed(Kg)
+            Ig = np.random.choice([0, 255], size=(self.width, self.height), p=[0.5, 0.5])
+            np.random.seed(Kb)
+            Ib = np.random.choice([0, 255], size=(self.width, self.height), p=[0.5, 0.5])
 
-    def 
+            r = self.image[:, :, 0]
+            g = self.image[:, :, 1]
+            b = self.image[:, :, 2]
+            return np.dstack((r ^ Ir, g ^ Ig, b ^ Ib))
+        else:
+            Kg = self.Kc
+            np.random.seed(Kg)
+            I = np.random.choice([0, 255], size=(self.width, self.height), p=[0.5, 0.5])
+            return I ^ self.image
+
+    def Shuffle(self):
+        image_np = self.NP()
+        r = image_np[:, :, 0]
+        g = image_np[:, :, 1]
+        b = image_np[:, :, 2]
+        if self.Ks == 0:
+            return np.dstack((r, g, b))
+        elif self.Ks == 1:
+            return np.dstack((r, b, g))
+        elif self.Ks == 2:
+            return np.dstack((g, r, b))
+        elif self.Ks == 3:
+            return np.dstack((g, b, r))
+        elif self.Ks == 4:
+            return np.dstack((b, r, g))
+        elif self.Ks == 5:
+            return np.dstack((b, g, r))
+
+    def apply(self):
+        if self.channels == 3:
+            choice = random.choice([self.Shuffle(), self.NP()])
+            return choice
+        else:
+            return self.NP()
+
+if __name__ == '__main__':
+    cifar10 = CIFAR10()
+    mnist = MNIST()
+    dataset = 'mnist'
+    for i in range(10):
+        image, label = mnist.dataset[i]
+        image = image.astype('uint8')
+        method = Pixel_Negative_Positive_Transformation(
+            image=image,
+            Kc=(2024, 2025, 2026),
+            Ks=random.randint(0, 5)
+        )
+
+        transfer_image = method.apply()
+        plt.imshow(transfer_image, cmap='gray')
+        plt.savefig(r'transformed datasets/{}_{}_{}_{}.png'.format(dataset, i, method.method_label, label))
